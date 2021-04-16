decf3e027e19f650763a9195e8ce5656934e2bfa
Glitch (ethereal-deep-astronaut) <none>
2021-04-16 07:08:16 +0000

    👫🏍 Checkpoint ./events/message.js:30832222/6297 ./komutlar/bot-onayla.js:30832222/1759 ./komutlar/bot-reddet.js:30832222/2115 ./komutlar/botekle.js:30832222/2508 ./komutlar/botlist-ayar.js:30832222/3394 ./komutlar/botlistyardımm.js:30832222/1213

---
+++ komutlar/botlistyardımm.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+
+exports.run = function(client, message) {
+const embed = new Discord.MessageEmbed()
+.setColor('RANDOM')
+.setTitle('Botlist Yardım Komutları')
+.setTimestamp()
+.addField('.bot-onayla botid sahipid ', 'Botu Onaylarsınız')
+.addField('.bot-reddet botid sahipid sebep', 'Botu Reddedersiniz')
+.addField('.bot-ekle', 'Bot eklersiniz')
+.addField('.botlist-ayar', 'Ayarlama Komutlarını Gösterir')
+.setImage('https://media.discordapp.net/attachments/769281758977458176/783299061012234260/standard_21.gif')
+.setFooter('Roliz', client.user.avatarURL())
+.setTimestamp()
+.setThumbnail(client.user.avatarURL())
+message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false, 
+  aliases: [], 
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'botlistyardım',
+  description: 'Tüm komutları gösterir.',
+  usage: 'yardım'
+};
