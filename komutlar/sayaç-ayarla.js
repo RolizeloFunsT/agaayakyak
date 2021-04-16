74e08e15d8668c4c7f82634f25b113698248f8f7
Glitch (ethereal-deep-astronaut) <none>
2021-04-16 10:33:31 +0000

    🍓🏉 Checkpoint ./komutlar/sayaç-ayarla.js:30832222/4835 ./index.js:30832222/3628 ./komutlar/otorol.js:30832222/1524

--- komutlar/sayaç-ayarla.js
+++ komutlar/sayaç-ayarla.js
@@ -1,99 +1,21 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
-const fs = require('fs')
- 
-exports.run = async (client, message, args) => {
-  if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`Bu Komutu Kullanabilmek İçin **Yönetici** İznine Sahip Olmalısın!`);
-        if(!args[0]) {
-                const embed = new Discord.RichEmbed()
-                        .setDescription(`:x: **Doğru Kullanım** sayaç <sayı> #kanal`)
-                        .setColor("#000000")
-                        .setTimestamp()
-                message.channel.send({embed})
-                return
-  }
- 
-        let profil = JSON.parse(fs.readFileSync("./sayac.json", "utf8"));
-  var mentionedChannel = message.mentions.channels.first();
-  const s1 = new Discord.RichEmbed()
-  .setDescription(':x: **Doğru Kullanım** sayaç <sayı> #kanal')
-  .setColor("#000000")
-                        .setTimestamp()
-  if (!mentionedChannel && args[0] !== "sıfırla") return message.channel.send(s1);
- 
- 
-        if(args[0] === "sıfırla") {
-                if(!profil[message.guild.id]) {
-                        const embed = new Discord.RichEmbed()
-                                .setDescription(`:x: Sayaç Daha Önce Ayarlanmamış! **Ayarlamak İçin** sayaç <sayı> #kanal`)
-                                .setColor("#000000")
-                                .setTimestamp()
-                        message.channel.send({embed})
-                        return
-                }
-                delete profil[message.guild.id]
-                fs.writeFile("./sayac.json", JSON.stringify(profil), (err) => {
-                        console.log(err)
-                })
-                const embed = new Discord.RichEmbed()
-                        .setDescription(`:x: Sayaç Kapatıldı!`)
-                        .setColor("#000000")
-                        .setTimestamp()
-                message.channel.send({embed})
-                return
-        }
- 
-        if(isNaN(args[0])) {
-                const embed = new Discord.RichEmbed()
-                        .setDescription(`:x: Lütfen, geçerli bir sayı belirtiniz! sayaç <sayı> #kanal`)
-                        .setColor("#000000")
-                        .setTimestamp()
-                message.channel.send({embed})
-                return
-        }
- 
-        if(args[0] <= message.guild.memberCount) {
-                const embed = new Discord.RichEmbed()
-                        .setDescription(`Lütfen, [${message.guild.memberCount}] rakamlı sayıdan daha yüksek bir değer belirtiniz! `)
-                        .setColor("#000000")
-                        .setTimestamp()
-                message.channel.send({embed})
-                return
-        }
- 
-        if(!profil[message.guild.id]){
-                profil[message.guild.id] = {
-                        sayi: args[0],
-      kanal: mentionedChannel.id
-                };
-        }
-       
-        profil[message.guild.id].sayi = args[0]
-  profil[message.guild.id].kanal = mentionedChannel.id
-       
-        fs.writeFile("./sayac.json", JSON.stringify(profil), (err) => {
-                console.log(err)
-        })
- 
-        const embed = new Discord.RichEmbed()
-                .setDescription(`Sayaç, başarılı bir şekilde \`${args[0]}\` olarak ayarlandı, sayaç kanalı ise ${mentionedChannel} olarak ayarlandı!`)
-                .setFooter('Render Bot', client.user.avatarURL)
-                .setColor("#000000")
-                .setTimestamp()
-        message.channel.send({embed})
+const Discord = require('discord.js');
+const qdb = require('quick.db');
+exports.run = async(client, message, args) => {
+    if(args[0] !== "sıfırla"){
+      var kanal = message.mentions.channels.first();
+      if(!kanal) return message.reply("kanalı ayarla aq")
+      var sayı = args[1]
+      if(!sayı) return message.reply("sayacı kafamdan random mu tutucam oç sayı belirle qweqweqw")
+      if(isNaN(sayı)) return message.reply("kanka sayı gir dedik sayı **SAYI** HARF DEİL GERİZEKALI")
+      if(sayı < message.guild.memberCount) return message.reply("Sunucun hedef sayaçtan daha büyük amk")
+      qdb.set(`sayackanali_${message.guild.id}`, kanal.id)
+      qdb.set(`sayachedef_${message.guild.id}`, sayı)
+      return message.reply("Sayaçı ayarladım bro!")
+    }
+    if(args[0] === "sıfırla"){
+      qdb.delete(`sayackanali_${message.guild.id}`)
+      qdb.delete(`sayachedef_${message.guild.id}`)
+      return message.reply("sıfırladm aşkm muah")
+    }
 }
- 
-exports.conf = {
-        enabled: true,
-        guildOnly: true,
-        aliases: ['sayaç-ayarla'],
-        permLevel: 2,
-        kategori: "moderasyon"
-}
- 
-exports.help = {
-        name: 'sayaç',
-        description: 'Sayaç, ayarlar!',
-        usage: 'sayaç-ayarla [sayı/sıfırla] [kanal]'
-}
-   
+module.exports.conf = {permLevel: 3, aliases: []}; module.exports.help = {name: "sayaç"}
