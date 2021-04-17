e75994b1ecb9f596bf06f95005d3401aa7e9cf27
Glitch (ethereal-deep-astronaut) <none>
2021-04-17 08:46:10 +0000

    🏣🐁 Checkpoint ./komutlar/eglence.js:30832222/1050

---
+++ komutlar/eglence.js
@@ -0,0 +1,23 @@
+const discord = require('discord.js');
+exports.run = async(client, message, args) => {
+    const embed = new discord.MessageEmbed()
+    .setTitle('Roliz Yardım')
+    .setColor('GOLD')
+    .setThumbnail('https://cdn.glitch.com/2fa30168-89cc-4986-b626-93d09bc61fbe%2Fb4b6052a1d4c2a17a53a5d77c5945c1c.png?v=1618560560952')
+     .addField('.espri', 'espri yapar', true)
+    .setImage('https://cdn.glitch.com/2fa30168-89cc-4986-b626-93d09bc61fbe%2Fstandard.gif?v=1618560688199')
+
+message.channel.send(embed)
+};
+exports.conf = {
+enabled: true,
+guildOnly: false,
+permLevel: 0,
+aliases: ['eglence']
+
+
+
+};
+exports.help = {
+name: "eğlence"
+};
