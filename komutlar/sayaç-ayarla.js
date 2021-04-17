2c6a0daa79164b377d68f0a62ff77e201608d951
Glitch (ethereal-deep-astronaut) <none>
2021-04-17 08:57:18 +0000

    👖🌟 Checkpoint ./komutlar/sayaç-ayarla.js:30832222/238

--- komutlar/sayaç-ayarla.js
+++ komutlar/sayaç-ayarla.js
@@ -3,11 +3,11 @@ const qdb = require('quick.db');
 exports.run = async(client, message, args) => {
     if(args[0] !== "sıfırla"){
       var kanal = message.mentions.channels.first();
-      if(!kanal) return message.reply("kanalı ayarla aq")
+      if(!kanal) return message.reply("Kanalı Girin!")
       var sayı = args[1]
-      if(!sayı) return message.reply("sayacı kafamdan random mu tutucam oç sayı belirle qweqweqw")
-      if(isNaN(sayı)) return message.reply("kanka sayı gir dedik sayı **SAYI** HARF DEİL GERİZEKALI")
-      if(sayı < message.guild.memberCount) return message.reply("Sunucun hedef sayaçtan daha büyük amk")
+      if(!sayı) return message.reply("Lütfen Bir Hedef Girin!")
+      if(isNaN(sayı)) return message.reply("Lütfen Sayı Girin!")
+      if(sayı < message.guild.memberCount) return message.reply("Sunucun hedef sayaçtan daha büyük olamaz!")
       qdb.set(`sayackanali_${message.guild.id}`, kanal.id)
       qdb.set(`sayachedef_${message.guild.id}`, sayı)
       return message.reply("Sayaçı ayarladım bro!")
@@ -15,7 +15,7 @@ exports.run = async(client, message, args) => {
     if(args[0] === "sıfırla"){
       qdb.delete(`sayackanali_${message.guild.id}`)
       qdb.delete(`sayachedef_${message.guild.id}`)
-      return message.reply("sıfırladm aşkm muah")
+      return message.reply("Sayacı Sıfırladım!")
     }
 }
 module.exports.conf = {permLevel: 3, aliases: []}; module.exports.help = {name: "sayaç"}
