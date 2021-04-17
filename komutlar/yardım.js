3363fc6e4ee6ed0fdeac217a9fb22b050096b4ea
Glitch (ethereal-deep-astronaut) <none>
2021-04-17 09:09:20 +0000

    🚂🐚 Checkpoint ./komutlar/yardım.js:30832222/87

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -2,12 +2,13 @@ const discord = require('discord.js');
 exports.run = async(client, message, args) => {
     const embed = new discord.MessageEmbed()
     .setTitle('Roliz Yardım')
-    .setColor('GOLD')
+    .setColor('BLUE')
     .setThumbnail('https://cdn.glitch.com/2fa30168-89cc-4986-b626-93d09bc61fbe%2Fb4b6052a1d4c2a17a53a5d77c5945c1c.png?v=1618560560952')
      .addField('.aboneyardım', 'abone yardım menüsü', true)
     .addField('.istatistik', 'botun istatistikleri', true)
     .addField('.moderasyon', 'moderasyon menüsü', true)
         .addField('.botlist', 'botlist menüsü', true)
+     .addField('.eğlence', 'eğlence menüsü', true)
     .setImage('https://cdn.glitch.com/2fa30168-89cc-4986-b626-93d09bc61fbe%2Fstandard.gif?v=1618560688199')
 
 message.channel.send(embed)
